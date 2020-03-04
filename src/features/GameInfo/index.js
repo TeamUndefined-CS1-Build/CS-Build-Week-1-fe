7e5e64696ba01bbfff9c255d58b1757c871ec397
Lorenzo Simpson <lorenzosimp@gmail.com>
2020-03-04 16:28:24 -0500

    changes color of grass
    Co-authored by <davidinden@gmail.com> <apst12012@gmail.com>

--- src/features/GameInfo/index.js
+++ src/features/GameInfo/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+const styles = {
+    text: {
+        color: 'white',
+    }
+}
+
 const GameInfo = props => {
     const currentRoom = useSelector(state => state.player.currentRoom)
 
     console.log(currentRoom)
     return (
-        <div>
+        <div style={styles.text}>
             <h1>You're in {currentRoom.name}!</h1>
+            <p>{currentRoom.desc}</p>
             </div>
     )
 }
--- src/features/world/index.js
+++ src/features/world/index.js
@@ -6,21 +6,35 @@ import GameInfo from '../GameInfo/index.js';
 import { tiles } from '../../data/maps/1'
 import store from '../../config/store'
 
+const styles = {
+    container: {
+        fontFamily: 'Press Start 2P',
+        height: '100vh',
+        display: 'block',
+        transition: '0.5s',
+        background: 'linear-gradient(rgba(0, 0, 0, 0.80), rgba(0, 0, 0, 0.95)), url(https://images.unsplash.com/photo-1440581572325-0bea30075d9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+    },
+}
 
 const World = props => {
     return (
+        <container style={styles.container}>
         <div
             style={{
                 position: 'relative',
                 width: '800px',
                 height: '400px',
-                margin: '20px auto'
+                margin: '0 auto',
+                // paddingTop: '50px',
             }}
         >
             <Map />
             <Player />
             <GameInfo />
         </div>
+        </container>
     )
 }
 
